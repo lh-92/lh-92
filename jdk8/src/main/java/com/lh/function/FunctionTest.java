3face24231e8b202e5f5a713ce208204a10ac413
lh <123456>
2017-05-05 23:18:02 +0800

    one

---
+++ jdk8/src/main/java/com/lh/function/FunctionTest.java
@@ -0,0 +1,12 @@
+package com.lh.function;
+
+import java.util.Arrays;
+
+/**
+ * Created by lh on 2017/5/5.
+ */
+public class FunctionTest {
+    public static void main(String[] args) {
+        Arrays.asList("zhangsan","lisi","wangwu");
+    }
+}
